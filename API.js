9c7cbfaa29030184789e7aed9330823f7e31c5a4
yuta <yuta20030801@icloud.com>
2017-08-22 08:57:42 +0900

    [change]ファイル名をクラス名に変更

---
+++ API.js
@@ -0,0 +1,33 @@
+const YouTube      = require('./youtube.js'),
+			TwitCasting  = require('./twitcasting.js'),
+			EventEmitter = require('events').EventEmitter,
+			util         = require('./util.js');
+
+class API extends EventEmitter {
+	constructor(type) {
+		super();
+		switch (type) {
+			case 'youtube':     this.api = new YouTube();     break;
+			case 'twitcasting': this.api = new TwitCasting(); break;
+			default: throw new Error('Type is not appropriate');
+		}
+		this.api.on('error', data => { this.emit('error', data) });
+		this.api.on('ready', data => { this.emit('ready', data) });
+		this.api.on('json',  data => { this.emit('json',  data) });
+		this.api.on('chat',  data => { this.emit('chat',  data) });
+	}
+
+	authorize() {
+		this.api.authorize();
+	}
+
+	listen(timeout) {
+		this.api.listen(timeout);
+	}
+
+	send(message) {
+		this.api.send(message);
+	}
+}
+
+module.exports = API;
