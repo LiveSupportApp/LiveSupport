dda4874eeffc560c87c6eb4ffe09613fdbdf27d3
yuta <yuta20030801@icloud.com>
2017-08-25 16:06:41 +0900

    [add]サーバー認証の追加

---
+++ API/TwitCasting/Server.js
@@ -0,0 +1,37 @@
+const credential = require('./credential/server.json');
+const http = require('http');
+const url = require('url');
+const {shell} = require('electron');
+const request = require('request');
+
+class Server {
+  constructor() {
+    this.server = http.createServer();
+  }
+
+  getNewToken(callback) {
+    let authUrl = `https://ssl.twitcasting.tv/oauth_confirm.php?client_id=${credential.client_id}&response_type=code`;
+    shell.openExternal(authUrl);
+    this.server.listen('localhost',7170);
+    this.server.on('request', (req, res) => {
+      this.handler(req, res, callback);
+    });
+  }
+
+  handler(req, res, callback) {
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
+    let qs = url.parse(req.url, true).query;
+    let message = 'LiveSupport\n';
+    if (qs.code) {
+      message += '認証しました';
+      callback(qs.code);
+    } else if (qs.result == 'denied') {
+      message += 'アクセスが拒否されました';
+    }
+    res.write(`${message}\nこれでこのウィンドウまたはタブを閉じてもかまいません。`);
+    res.end();
+    this.server.close();
+  }
+}
+
+module.exports = Server;
---
+++ API/YouTube/Server.js
@@ -0,0 +1,66 @@
+const credential = require('./credential/server.json');
+const http = require('http');
+const url = require('url');
+const {shell} = require('electron');
+const request = require('request');
+const googleAuth = require('google-auth-library');
+const auth = new googleAuth();
+const oauth2Client = new auth.OAuth2(
+        credential.web.client_id,
+        credential.web.client_secret,
+        credential.web.redirect_uris[0]
+      );
+
+class Server {
+  constructor() {
+    this.server = http.createServer();
+  }
+
+  authorize(data, callback) {
+    oauth2Client.credentials = data;
+    callback(oauth2Client);
+  }
+
+  getNewToken(callback) {
+    let authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'online',
+      scope: 'https://www.googleapis.com/auth/youtube'
+    });
+    shell.openExternal(authUrl);
+    this.server.listen(7170);
+    this.server.on('request', (req, res) => {
+      this.handler(req, res, code => {
+        oauth2Client.getToken(code, (err, token) => {
+          if (err) {
+            Util.msgbox({
+              type: 'warning',
+              btns: ['再認証'],
+              msg: '認証できませんでした。',
+              detail: err.toString()
+            }, id => { this.getNewToken(callback); });
+          } else {
+            oauth2Client.credentials = token;
+            callback(oauth2Client, token);
+          }
+        });
+      });
+    });
+  }
+
+  handler(req, res, callback) {
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
+    let qs = url.parse(req.url, true).query;
+    let message = 'LiveSupport\n';
+    if (qs.code) {
+      message += '認証しました';
+      callback(qs.code);
+    } else if (qs.result == 'denied') {
+      message += 'アクセスが拒否されました';
+    }
+    res.write(`${message}\nこれでこのウィンドウまたはタブを閉じてもかまいません。`);
+    res.end();
+    this.server.close();
+  }
+}
+
+module.exports = Server;
