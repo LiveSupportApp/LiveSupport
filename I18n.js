0bb1b47ebfc3d13c47381515ead35f1665fe1876
yuta0801 <yuta20030801@icloud.com>
2017-09-28 17:40:34 +0900

    Make js language file to json

--- I18n.js
+++ I18n.js
@@ -4,8 +4,8 @@ const Util = require('Util')
 
 class I18n {
   constructor() {
-    this.main = Util.join(__dirname, '_locales/', 'en.js')
-    this.path = Util.join(__dirname, '_locales/', app.getLocale() + '.js')
+    this.main = Util.join(__dirname, '_locales/', 'en.json')
+    this.path = Util.join(__dirname, '_locales/', app.getLocale() + '.json')
     const filePath = (Util.isExist(this.path)) ? this.path : this.main
     this.language = Util.readJSON(filePath)
   }
